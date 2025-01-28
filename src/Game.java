4b0534d980fa8e1acabc9b555507f910604dc176
matt_woahman <matt_homan1@baylor.edu>
2025-01-28 11:43:41 -0600

    added game object and created steam api reading. JSON parser needs tweaking, likes to find END_OBJECT early on nested objects. Corrected for most games, but unexpected nested objects may still exist.

---
+++ src/Game.java
@@ -0,0 +1,5 @@
+class Game {
+    public String name;
+    public String description;
+    public String[] developers = new String[3];
+}
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,25 @@
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicReference;
+
 
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
         String apiKey = System.getenv("APIKEY");
-
-        String appId = "1173800";
+        String appId = "550";
         String url = "https://store.steampowered.com/api/appdetails?appids=" + appId;
         HttpClient client = HttpClient.newHttpClient();
+        Game game = new Game();
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
@@ -23,11 +29,67 @@ public class Main {
         client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)
                 .thenAccept(responseBody -> {
-                    String jsonResponse = responseBody;
-                    System.out.println(jsonResponse);
+                    jsonParser(responseBody, appId, game);
                 })
                 .join();
-        // System.out.println("Ben has afk'd in " + gameInput + " for " + (random.nextInt(10000)+1000) + " hours!");
+
+        }
+    private static void jsonParser(String jsonResponse, String appId, Game game) {
+        JsonFactory factory = new JsonFactory();
+
+        try (JsonParser parser = factory.createParser(jsonResponse))  {
+            while (!parser.isClosed()) {
+                JsonToken token = parser.nextToken();
+                if (token == null) {
+                    break;
+                }
+                if (token == JsonToken.FIELD_NAME && parser.getCurrentName().equals(appId)){
+                    parser.nextToken();
+
+                    while (parser.nextToken() != JsonToken.END_OBJECT){
+                        String dataFieldName = parser.getCurrentName();
+
+                        parser.nextToken();
+                        if ("name".equals(dataFieldName)) {
+                            game.name = parser.getValueAsString();
+                        } else if ("short_description".equals(dataFieldName)) {
+                            game.description = parser.getValueAsString();
+                        }  else if ("pc_requirements".equals(dataFieldName)) {
+                            while (parser.nextToken() != JsonToken.END_OBJECT){
+                                parser.nextToken();
+                            }
+                        }  else if ("mac_requirements".equals(dataFieldName)) {
+                            while (parser.nextToken() != JsonToken.END_OBJECT){
+                                parser.nextToken();
+                            }
+                        }  else if ("linux_requirements".equals(dataFieldName)) {
+                            while (parser.nextToken() != JsonToken.END_OBJECT){
+                                parser.nextToken();
+                            }
+                        }
+                        else if ("developers".equals(dataFieldName)) {
+                           while(parser.nextToken() != JsonToken.END_ARRAY){
+                               int i = 0;
+                               game.developers[i] = parser.getValueAsString();
+                               i++;
+                           }
+                        }
+                    }
+                    System.out.println("Name: " + game.name);
+                    System.out.println("Description: " + game.description);
+                    System.out.println("Developers: ");
+                    for (int i = 0; i < game.developers.length; i++ )
+                        if (game.developers[i] != null) {
+                            System.out.println(game.developers[i]);
+                        }
+                    return;
+                }
+            }
+        } catch (JsonParseException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
