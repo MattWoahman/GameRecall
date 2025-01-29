2d5b569ca0eb787e3c9e6b1feb63ef99a6da6c07
MattWoahman <Matt_Homan1@baylor.edu>
2025-01-28 23:01:48 -0600

    Created ability to query any steam games (must have a copy of steam api locally)

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,27 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         //String apiKey = System.getenv("APIKEY");
-        String appId = "10";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What game do you want the info for?");
+        String gameName = scanner.nextLine();
+        String appId = searchForString(gameName);
+
         String url = "https://store.steampowered.com/api/appdetails?appids=" + appId;
         HttpClient client = HttpClient.newHttpClient();
         SteamApp game = new SteamApp();
-
+        game.name = gameName;
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .GET()
@@ -18,10 +29,33 @@ public class Main {
 
         client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)
-                .thenAccept(responseBody -> SteamDataParse.jsonParser(responseBody, appId, game))
+                .thenAccept(SteamDataParse::jsonParser)
                 .join();
 
+    }
+    private static String searchForString(String targetString) {
+        JsonElement fileJson;
+        String appId = null;
+        try (FileReader file = new FileReader("steam.txt")) {
+            fileJson = JsonParser.parseReader(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+        JsonObject jsonObject = fileJson.getAsJsonObject();
+        JsonObject appList = jsonObject.getAsJsonObject("applist");
+        JsonArray appsArray = appList.getAsJsonArray("apps");
+
+        for (JsonElement appElement : appsArray){
+            JsonObject appObject = appElement.getAsJsonObject();
+            String appName = appObject.get("name").getAsString();
+            if (appName.equals(targetString)){
+                appId = appObject.get("appid").getAsString();
+                break;
+            }
+        }
+        return appId;
+    }
 
 }
 
--- src/SteamApp.java
+++ src/SteamApp.java
@@ -1,11 +1,21 @@
 class SteamApp {
-    public String name;
+    public static String name;
     public String short_description;
     public String[] developers = new String[10];
     public ReleaseDate release_date;
-
+    public String[] publishers = new String[10];
+    public Categories[] categories = new Categories[10];
+    public Genres[] genres = new Genres[10];
     static class ReleaseDate {
         String date;
         boolean coming_soon;
+    }
+    static class Categories {
+        String id;
+        String description;
+    }
+    static class Genres {
+        String id;
+        String description;
     }
 }
