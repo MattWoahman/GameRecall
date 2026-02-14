da90e8dffdfaabfd2c6c510d73e3f9d468adb961
Matt Homan <matthewhoman15@gmail.com>
2026-02-13 21:41:59 -0600

    Added player database caching + checking + removed repeat inserts

--- steam.py
+++ steam.py
@@ -8,7 +8,6 @@ api_key=os.getenv("STEAM_API_KEY")
 #hi matt
 # what up?
 # nothing much, just working on the steam.py file
-player_list = []
 
 class Game():
     name = ''
@@ -93,45 +92,75 @@ def steam_player_id(player_id):
     steamID = player_id
     api_key="0AA30FC317E4A3ADCA63BD9F0C13A273"
     steam_player_url = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + api_key + "&steamids=" + steamID
-    response = requests.get(steam_player_url)
-    report = response.json()
 
-    if len(report['response']['players']) !=0:
-        for player in report['response']['players']:
 
-            cur_player = Player()
-            cur_player.steam_id = player['steamid']
-            cur_player.steam_name = player['personaname']
-            cur_player.profile_url = player['profileurl']
-            cur_player.avatar = player['avatarfull']
-            cur_player.last_logoff = str(datetime.fromtimestamp(int(player['lastlogoff'])))
-            cur_player.time_created = str(datetime.fromtimestamp(int(player['timecreated'])))
-            if player.get('realname'):
-                cur_player.real_name = player['realname']
-            if player.get('loccountrycode'):
-                cur_player.country_code = player['loccountrycode']
-            if player.get('gameid'):
-                cur_player.gameid
-            if player.get('gameid'):
-                cur_player.gameid = player['gameid']
-            if player.get('gameserverip'):
-                cur_player.gameserverip = player.get('gameserverip')
-            if player.get('gameextrainfo'):
-                cur_player.gameextrainfo = player.get('gameextrainfo')
-            
-            play_time_get(cur_player)
+    connection = psycopg2.connect(database="gamerecall", user="gamerecalldba", password="test", host="192.168.1.182", port=5432)    
+    cursor = connection.cursor()
+    sql_query = "SELECT * FROM players WHERE steam_id = " + player_id+"::VARCHAR"
+    cursor.execute(sql_query)
+    record = cursor.fetchone()
+
+    print(record)
+    if record != None:
+        cur_player = Player()
+        cur_player.steam_id = record[0]
+        cur_player.steam_name = record[13]
+        cur_player.profile_url = record[1]
+        cur_player.avatar = record[2]
+        cur_player.last_logoff = record[3]
+        cur_player.time_created = record[4]
+        cur_player.first_game = record[10]
+        cur_player.second_game = record[11]
+        cur_player.third_game = record[12]
+        ''' I'll come back to this
+        if player.get('realname'):
+            cur_player.real_name = player['realname']
+        if player.get('loccountrycode'):
+            cur_player.country_code = player['loccountrycode']
+        if player.get('gameid'):
+            cur_player.gameid
+        if player.get('gameid'):
+            cur_player.gameid = player['gameid']
+        if player.get('gameserverip'):
+            cur_player.gameserverip = player.get('gameserverip')
+        if player.get('gameextrainfo'):
+            cur_player.gameextrainfo = player.get('gameextrainfo')
+        '''
         
+    else:
+        response = requests.get(steam_player_url)
+        report = response.json()
+        if len(report['response']['players']) !=0:
+            for player in report['response']['players']:
+                
+                cur_player = Player()
+                cur_player.steam_id = player['steamid']
+                cur_player.steam_name = player['personaname']
+                cur_player.profile_url = player['profileurl']
+                cur_player.avatar = player['avatarfull']
+                cur_player.last_logoff = str(datetime.fromtimestamp(int(player['lastlogoff'])))
+                cur_player.time_created = str(datetime.fromtimestamp(int(player['timecreated'])))
+                if player.get('realname'):
+                    cur_player.real_name = player['realname']
+                if player.get('loccountrycode'):
+                    cur_player.country_code = player['loccountrycode']
+                if player.get('gameid'):
+                    cur_player.gameid
+                if player.get('gameid'):
+                    cur_player.gameid = player['gameid']
+                if player.get('gameserverip'):
+                    cur_player.gameserverip = player.get('gameserverip')
+                if player.get('gameextrainfo'):
+                    cur_player.gameextrainfo = player.get('gameextrainfo')
+                
+                play_time_get(cur_player)
+                
+                cursor.execute("INSERT INTO players (steam_id, profile_url, avatar, last_logoff, time_created, first_game, second_game, third_game, steam_name) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)", 
+                (cur_player.steam_id, cur_player.profile_url,cur_player.avatar,cur_player.last_logoff,cur_player.time_created,cur_player.first_game,cur_player.second_game,cur_player.third_game,cur_player.steam_name))
+                connection.commit()
+                connection.close()
 
-            player_list.append(cur_player)
-
-        connection = psycopg2.connect(database="gamerecall", user="gamerecalldba", password="test", host="thehomans.org", port=5432)    
-        cursor = connection.cursor()
-        cursor.execute("INSERT INTO players (steam_id, profile_url, avatar, last_logoff, time_created, first_game, second_game, third_game, steam_name, player_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,1)", 
-                    (cur_player.steam_id, cur_player.profile_url,cur_player.avatar,cur_player.last_logoff,cur_player.time_created,cur_player.first_game,cur_player.second_game,cur_player.third_game,cur_player.steam_name))
-        connection.commit()
-        connection.close()
-        print("sent!")
-        return cur_player
+    return cur_player
     
     
 
